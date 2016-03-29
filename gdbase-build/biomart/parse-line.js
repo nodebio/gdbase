be7456d68a79ebdb39ff7e37904231d9d23483fb
jmjuanes <josemijuanes@gmail.com>
2016-03-29 12:50:01 +0200

    Parse source line

---
+++ gdbase-build/biomart/parse-line.js
@@ -0,0 +1,31 @@
+//Function for parse a file line
+function ParseLine(line, cols, exclude)
+{
+	//Check the exclude
+	if(typeof exclude === 'undefined'){ var exclude = []; }
+
+	//Create the new object
+	var obj = {};
+
+	//Read all the cols
+	for(var i = 0; i < cols.length; i++)
+	{
+		//Get the column name
+		var col = cols[i].id;
+
+		//Check for exclude
+		if(exclude.indexOf(col) < 0){ continue; }
+
+		//Check for empty
+		if(line[i] === '' || line[i] === ' '){ continue; }
+
+		//Save the object
+		obj[col] = line[j];
+	}
+
+	//Return the new object
+	return obj;
+}
+
+//Exports
+module.exports = ParseLine;
