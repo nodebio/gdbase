beed57f0ec674284251518bd0e685bf43f989c73
jmjuanes <josemijuanes@gmail.com>
2016-04-27 16:49:01 +0200

    Fix genes download

--- gdbase-client/biomart_genes/import.js
+++ gdbase-client/biomart_genes/import.js
@@ -1,9 +1,11 @@
 //Import dependencies
 var execSync = require('child_process').execSync;
+var mkdirp = require('mkdirp');
+var path = require('path');
 
 //Import utils
-var UtilsFile = require('../utils/file.js');
 var UtilsBiomart = require('../utils/biomart.js');
+var UtilsData = require('../utils/data.js');
 
 //Function for import from biomart
 module.exports = function(opt)
@@ -14,42 +16,46 @@ module.exports = function(opt)
 	//Parse the assembly
 	opt.assembly = opt.assembly.toLowerCase();
 
-	//Get the output file
-	var output = UtilsFile.Biomart(opt.specie, opt.assembly, opt.import);
+	//Get the specie info
+	var Specie = require('../species/' + opt.specie + '.json');
 
-	//Get the command
-	var command = UtilsBiomart.Command();
+	//Get the source folder
+	var folder = UtilsData.Source(opt.specie, opt.assembly, 'genes');
 
-	//Check the specie and assembly
-	if(opt.specie === 'hsapiens' && opt.assembly === 'grch37')
+	//Create the folder
+	mkdirp.sync(folder);
+
+	//Read all the features to download
+	for(var i = 0; i < Specie.features.length; i++)
 	{
-		//Replace in the command
-		command = command.replace('{assembly}', 'grch37');
+		//Get the feature
+		var feature = Specie.features[i];
+
+		//Get the command
+		var command = UtilsBiomart.Command(opt.specie, opt.assembly);
+
+		//Get the query object
+		var query = require('./query/' + feature + '.json');
+
+		//Get the xml
+		var xml = UtilsBiomart.XML(query, opt.specie);
+
+		//Get the output file
+		var output = path.join(folder, UtilsData.SourceFile('biomart', 'genes', { feature: feature }));
+
+		//Replace the xml
+		command = command.replace('{xml}', xml);
+
+		//Replace the output file
+		command = command.replace('{output}', output);
+
+		//Show in console
+		console.log('Downloading ' + feature + ' for ' + opt.specie + '...');
+
+		//Run the import
+		var log = execSync(command, { stdio: [ 0, 0, 1 ] });
 	}
-	else
-	{
-		//Relace by default
-		command = command.replace('{assembly}', 'www');
-	}
-
-	//Get the query object
-	var query = require('./query/' + opt.import + '.json');
-
-	//Get the xml
-	var xml = UtilsBiomart.XML(query, opt.specie);
-
-	//Replace the xml
-	command = command.replace('{xml}', xml);
-
-	//Replace the output file
-	command = command.replace('{output}', output);
-
-	//Show in console
-	console.log('Wait...');
-
-	//Run the import
-	var log = execSync(command);
 
 	//Show confirmation
-	console.log('Saved as ' + output);
+	console.log('Download completed for ' + opt.specie + ' ' + opt.assembly);
 };
