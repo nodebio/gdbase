9d39f154f49320ea4d2e21aac34fb35f9d50b748
jmjuanes <josemijuanes@gmail.com>
2016-04-29 18:33:13 +0200

    Update biomart build

--- gdbase-client/biomart-build.js
+++ gdbase-client/biomart-build.js
@@ -1,18 +1,14 @@
 //Biomart build
-//Usage: node biomart-build.js --specie <> --assembly <> [--buildGenes ][--buildTranscripts][--buildExons][--BuildGenCode]
+//Usage: node biomart-build.js --specie <SPECIE> --assembly <ASSEMBLY> --dataset <DATASET>
 
 //Import dependencies
 var commandLineArgs = require('command-line-args');
-var fs = require('fs');
 
 //Initialize the command line arguments options
 var cli = commandLineArgs([
   { name: 'specie', type: String },
   { name: 'assembly', type: String },
-  { name: 'buildGenes', alias: 'g', type: Boolean },
-  { name: 'buildTranscripts', alias: 't', type: Boolean },
-  { name: 'buildExons', alias: 'e', type: Boolean },
-  { name: 'buildGencode', type: Boolean }
+  { name: 'dataset', type: String }
 ]);
 
 //Get the command line args
@@ -24,14 +20,8 @@ if(typeof opt.specie === 'undefined'){ return console.error('No specie provided.
 //Check the assembly
 if(typeof opt.assembly === 'undefined'){ return console.error('No assembly provided...'); }
 
-//Check the build transcripts
-if(typeof opt.buildTranscripts === 'undefined'){ opt.BuildTranscripts = false; }
-
-//Check the build exons
-if(typeof opt.buildExons === 'undefined'){ opt.buildExons = false; }
-
-//Check the build genecode
-if(typeof opt.buildGencode === 'undefined'){ opt.buildGencode = false; }
+//Check the dataset option
+if(typeof opt.dataset === 'undefined'){ return console.error('No dataset provided...'); }
 
 //Build
-require('./biomart/build.js')(opt);
+require('./biomart_' + opt.dataset + '/build.js')(opt);
